019b76e14c8817175b9377341c861f28af40a7a6
macortinas99 <matthewcortinas22@gmail.com>
2023-05-22 20:56:59 -0500

    installed mapbox and got basic working map component

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Map from './components/Map'
 import Navbar from './components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -12,8 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <Navbar />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
